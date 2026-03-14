13253220be1befbdcf026a1942de9ddb2371d500
Stachuh <stachu.games.bbc@gmail.com>
2026-03-15 00:15:54 +0100

    test

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gs)m)_bl3s#ulix&c+qb_feg8-)xd$h1m%x%xghtzlgb17mf3y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.50.143',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
