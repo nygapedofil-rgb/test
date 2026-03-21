359b2c831c830b5cfc8590d759fffb3b5167181b
Stachuh <stachu.games.bbc@gmail.com>
2026-03-21 16:21:03 +0100

    test

--- chat/urls.py
+++ chat/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('api/login/',views.api_login,),
     path('api/set_public_key/',views.api_set_public_key),
+    path('api/get_public_key/',views.api_get_public_key),
 ]
